042c80991eb4670d6e6ffd7667e529c639bc4750
Bob German <Bob.German@microsoft.com>
2023-03-08 11:10:34 -0800

    Upgrade preview card to thumbnail

--- Lab02-IntegrateBusinessData/src/NorthwindSuppliers/bot/messageExtensions/supplierME.js
+++ Lab02-IntegrateBusinessData/src/NorthwindSuppliers/bot/messageExtensions/supplierME.js
@@ -1,6 +1,25 @@
 const axios = require("axios");
 const { CardFactory } = require("botbuilder");
 
+const COUNTRY_CODES = {
+    "australia": "au",
+    "brazil": "br",
+    "canada": "ca",
+    "denmark": "dk",
+    "france": "fr",
+    "germany": "de",
+    "finland": "fi",
+    "italy": "it",
+    "japan": "jp",
+    "netherlands": "nl",
+    "norway": "no",
+    "singapore": "sg",
+    "spain": "es",
+    "sweden": "se",
+    "uk": "gb",
+    "usa": "us"
+}
+
 class SupplierME {
 
     // Get suppliers given a query
@@ -8,14 +27,22 @@ class SupplierME {
 
         try {
             const response = await axios.get(
-                `https://services.odata.org/V4/Northwind/Northwind.svc/Suppliers?$filter=contains(tolower(CompanyName),tolower('${query}'))&$top=8`
+                `https://services.odata.org/V4/Northwind/Northwind.svc/Suppliers` +
+                `?$filter=contains(tolower(CompanyName),tolower('${query}'))` +
+                `&$orderby=CompanyName&$top=8`
             );
 
             const attachments = [];
             response.data.value.forEach((supplier) => {
+
+                // Free flag images from https://flagpedia.net/
+                const flagUrl = `https://flagcdn.com/48x36/${COUNTRY_CODES[supplier.Country.toLowerCase()]}.png`;
+
                 const heroCard = CardFactory.heroCard(supplier.CompanyName);
-                const preview = CardFactory.heroCard(supplier.CompanyName);
-                preview.content.tap = {
+                const preview = CardFactory.thumbnailCard(supplier.CompanyName, 
+                    `${supplier.City}, ${supplier.Country}`, [flagUrl]);
+
+                    preview.content.tap = {
                     type: "invoke",
                     value: {
                         queryType: 'supplierME',
