a0655e6e4e7a6280a23e889f78457d214b9a10bc
Bob German <Bob.German@microsoft.com>
2023-03-09 16:12:09 -0800

    ME now shows adaptive cards with templating

--- Lab02-IntegrateBusinessData/src/NorthwindSuppliers/bot/messageExtensions/supplierME.js
+++ Lab02-IntegrateBusinessData/src/NorthwindSuppliers/bot/messageExtensions/supplierME.js
@@ -1,24 +1,6 @@
 const axios = require("axios");
-const { CardFactory, CardAction } = require("botbuilder");
-
-const COUNTRY_CODES = {
-    "australia": "au",
-    "brazil": "br",
-    "canada": "ca",
-    "denmark": "dk",
-    "france": "fr",
-    "germany": "de",
-    "finland": "fi",
-    "italy": "it",
-    "japan": "jp",
-    "netherlands": "nl",
-    "norway": "no",
-    "singapore": "sg",
-    "spain": "es",
-    "sweden": "se",
-    "uk": "gb",
-    "usa": "us"
-}
+const ACData = require("adaptivecards-templating");
+const { CardFactory } = require("botbuilder");
 
 class SupplierME {
 
@@ -36,7 +18,8 @@ class SupplierME {
             response.data.value.forEach((supplier) => {
 
                 // Free flag images from https://flagpedia.net/
-                const flagUrl = `https://flagcdn.com/48x36/${COUNTRY_CODES[supplier.Country.toLowerCase()]}.png`;
+                const flagUrl = this.#getFlagUrl(supplier.Country);
+                const imageUrl = `https://picsum.photos/seed/${supplier.SupplierID}/300`;
 
                 const itemAttachment = CardFactory.heroCard(supplier.CompanyName);
                 const previewAttachment = CardFactory.thumbnailCard(supplier.CompanyName,
@@ -46,9 +29,19 @@ class SupplierME {
                     type: "invoke",
                     value: {    // Values passed to selectItem when an item is selected
                         queryType: 'supplierME',
-                        name: supplier.CompanyName,
-                        description: supplier.ContactName,
-                        flagUrl: flagUrl
+                        SupplierID: supplier.SupplierID,
+                        flagUrl: flagUrl,
+                        imageUrl: imageUrl,
+                        Address: supplier.Address,
+                        City: supplier.City,
+                        CompanyName: supplier.CompanyName,
+                        ContactName: supplier.ContactName,
+                        ContactTitle: supplier.ContactTitle,
+                        Country: supplier.Country,
+                        Fax: supplier.Fax,
+                        Phone: supplier.Phone,
+                        PostalCode: supplier.PostalCode,
+                        Region: supplier.Region
                     },
                 };
                 const attachment = { ...itemAttachment, preview: previewAttachment };
@@ -62,13 +55,44 @@ class SupplierME {
     };
 
     selectItem = (selectedValue) => {
-        const heroCard = CardFactory.heroCard(selectedValue.name,
-            selectedValue.description,
-            [ selectedValue.flagUrl ]
-            );
-        return heroCard;
-    }
 
-};
+        // Read card from JSON file
+        const templateJson = require('./supplierCard.json');
+        const template = new ACData.Template(templateJson);
+        const card = template.expand({
+            $root: selectedValue
+        });
+
+        const resultCard = CardFactory.adaptiveCard(card);
+
+        return resultCard;
+    };
+
+    // Get a flag image URL given a country name
+    #getFlagUrl = (country) => {
+
+        const COUNTRY_CODES = {
+            "australia": "au",
+            "brazil": "br",
+            "canada": "ca",
+            "denmark": "dk",
+            "france": "fr",
+            "germany": "de",
+            "finland": "fi",
+            "italy": "it",
+            "japan": "jp",
+            "netherlands": "nl",
+            "norway": "no",
+            "singapore": "sg",
+            "spain": "es",
+            "sweden": "se",
+            "uk": "gb",
+            "usa": "us"
+        };
+
+        return `https://flagcdn.com/32x24/${COUNTRY_CODES[country.toLowerCase()]}.png`;
+
+    };
+}
 
 module.exports.SupplierME = new SupplierME();
